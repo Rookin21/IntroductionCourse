95a599ae45c9d5e551bc35f1623bf27e5cfe2dfb
Almas <Crazynick21@gmail.com>
2022-03-05 22:14:14 +0300

    Подготовка места.

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -6,17 +6,6 @@ namespace IntroductionCourse
     {
         static void Main(string[] args)
         {
-            // Вывод на консоль сообщения
-            Console.WriteLine("Введите имя пользователя:");
-
-            // Ввод пользователем имени
-            string user_name = Console.ReadLine();
-
-            // Вывод сообщения используя структуру текущей даты
-            Console.WriteLine($"Привет, {user_name}, сегодня {DateTime.Today.ToShortDateString()}");
-
-            // Ожидание нажатия клавиши Enter
-            Console.Read();
 
         }
     }
