3fac25ff39c86e1fff2280d8cbd36444f0f4746d
Almas <Crazynick21@gmail.com>
2022-03-08 22:46:32 +0300

    Подготовка ДЗ. Урок 3. Задание 3.

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -6,26 +6,19 @@ namespace IntroductionCourse
     {
         static void Main(string[] args)
         {
-            // Создание массива
-            string[,] phoneBook = 
-            { 
-                { "Александр", " +1 111 111 11 11" },
-                { "Анатолий", " +2 222 222 22 22" },
-                { "Евгений", " +3 333 333 33 33" },
-                { "Максим", " +4 444 444 44 44" },
-                { "Олег", " +5 555 555 55 55" }
-            };
+            // Ввод пользователем текста
+            string userText = Console.ReadLine();
+            
+            char [] reverseText = userText.ToCharArray();
 
-            // Запуск цикла для вывода данных массива
-            for (int i = 0; i < phoneBook.GetLength(0); i++)
+            Array.Reverse(reverseText);
+
+            for (int i = 0; i < reverseText.Length; i++)
             {
-                for (int j = 0; j < phoneBook.GetLength(1); j++)
-                {
-                    Console.Write($"{phoneBook[i,j]}");
-                }
-                Console.WriteLine();
+                Console.Write(reverseText[i]);
             }
-            Console.ReadLine();           
+
+            Console.Read();
         }
     }
 }
