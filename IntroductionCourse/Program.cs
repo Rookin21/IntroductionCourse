4670f08c28e8987293bf6ec2aeb497f5f1cb2ff8
Almas <Crazynick21@gmail.com>
2022-04-02 13:08:05 +0300

    Домашнее задание. Урок 4. Задание 1.

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -6,7 +6,37 @@ namespace IntroductionCourse
     {
         static void Main(string[] args)
         {
+            string firstName = GetFirstName();  // Вызов метода запроса Имени
+            string lastName = GetLastName();    // Вызов метода запроса Фамилии
+            string patronymic = GetPatronymic();    // Вызов метода запроса Фамилии
+            string FullName = GetFullName(firstName, lastName, patronymic); // Вызов метода вызова объединенной строки
+            Console.WriteLine(FullName); // Вывод данных на консоль
+        }
+                            
+        static string GetFullName(string firstName, string lastName, string patronymic)
+        {
+            return $"Привет, {lastName} {firstName} {patronymic}!";
+        }
 
+        static string GetLastName()
+        {
+            Console.Write("Фамилия: ");
+            string lastName = Console.ReadLine();
+            return lastName;
+        }
+
+        static string GetFirstName()
+        {
+            Console.Write("Имя: ");
+            string firstName = Console.ReadLine();
+            return firstName;
+        }
+
+        static string GetPatronymic()
+        {
+            Console.Write("Отчество: ");
+            string patronymic = Console.ReadLine();
+            return patronymic;
         }
     }
 }
