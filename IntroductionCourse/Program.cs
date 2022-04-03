15eff865485bb0bc5003990dfb837bec79b586b8
Almas <Crazynick21@gmail.com>
2022-04-03 10:24:14 +0300

    Домашнее задание. Урок 4. Задание 2.

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace IntroductionCourse
 {
@@ -6,7 +7,22 @@ namespace IntroductionCourse
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите числа через пробел");
+            // Разбиение введенных чисел на отдельные составляющие
+            int userNumbers = Sum(Console.ReadLine().Split().Sum(int.Parse));
 
+            Console.WriteLine(userNumbers);
+        }
+
+        // Метод с использование ключевого слова params для суммы введенных чисел
+        static int Sum(params int[] values)
+        {
+            int sum = 0;
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+            return sum;
         }
     }
 }
