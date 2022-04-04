4f22eab40f490f9f7a5ee1088bb6142441c1d191
Almas <Crazynick21@gmail.com>
2022-04-04 19:30:45 +0300

    Домашнее задание. Урок 5. Задание 1.

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.IO;
 
 namespace IntroductionCourse
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-           
+            Console.WriteLine("Введите текст: ");
+            string text = Console.ReadLine();
+            string path = "D:/Homework_GB/homework_lesson5_1.txt";
+            File.WriteAllText(path, text);
         }
     }
 }
