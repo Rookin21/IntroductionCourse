d454f32de09c554f5a38d30e002a7a8e10a9f8b1
Rookin21 <99610868+Rookin21@users.noreply.github.com>
2022-04-04 19:29:31 +0300

    Update Program.cs

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -6,18 +6,12 @@ namespace IntroductionCourse
     {
         static void Main()
         {
-            Console.Write("Введите крайнее порядковое число: ");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Fibonachi(number);
-            Console.WriteLine($"Число Фибоначчи: {Fibonachi(number)}");
-            Console.ReadKey();
+
         }
 
         static int Fibonachi(int number)
         {
-            if (number == 0 || number == 1) return number;
 
-            return Fibonachi(number - 1) + Fibonachi(number - 2);
         }
     }
 }
