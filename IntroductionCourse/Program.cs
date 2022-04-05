7ed60f89f1bfe9b34434c0890969c4eb18957675
Rookin21 <99610868+Rookin21@users.noreply.github.com>
2022-04-05 21:29:06 +0300

    Update Program.cs

--- IntroductionCourse/Program.cs
+++ IntroductionCourse/Program.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.Text;
 
 namespace IntroductionCourse
 {
@@ -8,33 +6,7 @@ namespace IntroductionCourse
     {
         static void Main()
         {
-            Console.WriteLine("Введите произвольной набор чисел 0...255");
-            string num = Console.ReadLine();
-
-            // Создание файла
-            using (FileStream fs = new FileStream("D:/homework.bin", FileMode.Create))
-            {
-                // Вызов класса для бинарной записи
-                using (BinaryWriter bw = new BinaryWriter(fs, Encoding.Default))
-                {
-                    // Запись
-                    bw.Write(num);
-                }
-            }
-            // Открытие файла
-            using (FileStream fs = new FileStream("D://HomeWork_GB/homework.bum", FileMode.Open))
-            {
-                // Вызов класса для бинарного чтения
-                using (BinaryReader br = new BinaryReader(fs, Encoding.Default))
-                {
-                    // Считывание
-                    string num2 = br.ReadString();
-
-                    // Вывести прочитание данные на экран для контроля
-                    Console.WriteLine("Чтение из файла: {0}", num2);
-                }
-            }
-            Console.Read();
+            
         }
     }
 }
